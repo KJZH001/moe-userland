acb8f489efbf8225be310b910ace1737b29e4863
KJZH001 <2802007619@qq.com>
2025-09-06 19:37:34 +0800

    干掉milvus

--- database/migrations/2024_09_03_115305_change_idx_type_on_oauth_faces.php
+++ database/migrations/2024_09_03_115305_change_idx_type_on_oauth_faces.php
@@ -9,7 +9,6 @@ return new class extends Migration
 
     public function __construct()
     {
-        $this->milvusSupport = new MilvusSupport;
     }
 
     /**
@@ -17,54 +16,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        $resp = $this->milvusSupport->releaseCollection(config('milvus.collection'));
-        if ($resp['code'] != MilvusSupport::CODE_SUCCESS) {
-            throw new Exception($resp['message']);
-        }
-
-        $resp = $this->milvusSupport->post('indexes/drop', [
-            'collectionName' => config('milvus.collection'),
-            'indexName' => 'face_id_idx',
-        ]);
-        if ($resp['code'] != MilvusSupport::CODE_SUCCESS) {
-            throw new Exception($resp['message']);
-        }
-
-        // 等待释放
-        echo "Waiting for release...\n";
-        while (true) {
-            $resp = $this->milvusSupport->post('indexes/list', [
-                'collectionName' => config('milvus.collection'),
-            ]);
-            if ($resp['code'] != MilvusSupport::CODE_SUCCESS) {
-                throw new Exception($resp['message']);
-            }
-
-            // 如果 $resp['data'] 里面没有 face_id_idx，说明已经释放成功
-            if (! in_array('face_id_idx', array_column($resp['data'], 'index_name'))) {
-                break;
-            }
-            sleep(5);
-        }
-
-        $resp = $this->milvusSupport->post('indexes/create', [
-            'collectionName' => config('milvus.collection'),
-            'indexParams' => [
-                (object) [
-                    'fieldName' => 'face_id',
-                    'indexName' => 'face_id_idx',
-                    'metricType' => '',
-                    'params' => [
-                        'index_type' => 'INVERTED',
-                    ],
-                ],
-            ],
-        ]);
-        if ($resp['code'] != MilvusSupport::CODE_SUCCESS) {
-            throw new Exception($resp['message']);
-        }
-
-        $this->milvusSupport->loadCollection(config('milvus.collection'));
+        
     }
 
     /**
@@ -72,29 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        $resp = $this->milvusSupport->post('indexes/drop', [
-            'collectionName' => config('milvus.collection'),
-            'indexName' => 'face_id_idx',
-        ]);
-        if ($resp['code'] != MilvusSupport::CODE_SUCCESS) {
-            throw new Exception($resp['message']);
-        }
-
-        $resp = $this->milvusSupport->post('indexes/create', [
-            'collectionName' => config('milvus.collection'),
-            'indexParams' => [
-                [
-                    'fieldName' => 'face_id',
-                    'indexName' => 'face_id_idx',
-                    'metricType' => '',
-                    'params' => [
-                        'index_type' => 'STL_SORT',
-                    ],
-                ],
-            ],
-        ]);
-        if ($resp['code'] != MilvusSupport::CODE_SUCCESS) {
-            throw new Exception($resp['message']);
-        }
+        
     }
 };
