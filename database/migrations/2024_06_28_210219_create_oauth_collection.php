a0206154f4f6cd6a7f987d277ed0a22c79215243
KJZH001 <2802007619@qq.com>
2025-09-06 19:35:36 +0800

    干掉milvus

--- database/migrations/2024_06_28_210219_create_oauth_collection.php
+++ database/migrations/2024_06_28_210219_create_oauth_collection.php
@@ -1,2 +1,29 @@
 <?php
-?>
+
+use App\Support\Milvus\MilvusSupport;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    private MilvusSupport $milvusSupport;
+
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+    }
+};
